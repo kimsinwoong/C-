48f4fc1f40f78b6a044435d927e13d2759049591
kimsinwoong <kimsinwoong7@gmail.com>
2025-09-16 15:49:59 +0900

    Add files via upload

---
+++ shop.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+int main() {
+	int su;
+	int id;
+	int gungun;
+	int pam=0;
+	float pamper;
+	int in[100];
+	int out[100];
+	int nam[100];
+
+	printf("상품 개수 입력 : ");
+	scanf_s("%d", &su);
+
+	printf("입고 수량 입력 : ");
+	for (int a = 0; a < su; a++) {
+		scanf_s("%d", &in[a]);
+	}
+	printf("당일 판매 수량 : ");
+	for (int a = 0; a < su; a++) {
+		scanf_s("%d", &out[a]);
+	}
+	for (int a = 0; a < su; a++) {
+		nam[a] = in[a] - out[a];
+	}
+
+	printf("ID 입력 : ");
+	scanf_s("%d", &id);
+
+	printf("%d\n", nam[id - 1]);
+	for (int a = 0; a < su; a++) {
+		printf("%d ", nam[a]);
+		printf(" ");
+	}
+	printf("\n");
+	for (int a = 0; a < su; a++) {
+		gungun += in[a];
+	}
+	for (int a = 0; a < su; a++) {
+		pam += out[a];
+	}
+	pamper = ((float)gungun / pam) * 100;
+
+	printf("총 판매량 : %d(판매율 : %.2f%%)", pam,pamper);
+
+	return 0;
+}
